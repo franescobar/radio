9083d304236d793cef3a400f345c7061d10bc8a7
German_Urena_A <gualixgo@gmail.com>
2020-08-08 10:51:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ from radio.basics import *
 
 circuito = Circuit("Proyecto simulacion 1")
 
+"""
+GND = circuito.addNode()
+Ra = circuito.addResistor(4, n1, n2)
+Va = circuito.addVDC(3.3, n1, GND)
+Ia = circuito.addVDC(3.3, n1, GND)
+"""
 GND = circuito.addNode()
 n1 = circuito.addNode()
 n2 = circuito.addNode()
 n3 = circuito.addNode()
 n4 = circuito.addNode()
-n5 = circuito.addNode()
 
-Ra = circuito.addResistor(4, n1, n2)
-Rb = circuito.addResistor(4, n2, n3)
-Rc = circuito.addResistor(8, n3, n5)
-Rd = circuito.addResistor(3, n3, n5)
-Re = circuito.addResistor(12, n5, n4)
-Rf = circuito.addResistor(7, n2, n4)
-Rg = circuito.addResistor(6, n4, GND)
+Ra = circuito.addResistor(800, n1, n2)
+Rb = circuito.addResistor(100, n4, GND)
+Rc = circuito.addResistor(10000, n2, n4)
+Rd = circuito.addResistor(1000, n2, n3)
 
-Va = circuito.addVDC(3.3, n1, GND)
-Vb = circuito.addVDC(5, n2, n4)
-Vc = circuito.addVDC(12, n3, n5)
-
+Ia = circuito.addIDC(0.14, GND, n1)
+Ib = circuito.addIDC(0.30, n3, GND)
 
 # --------- Fin de la descripción del circuito
 
